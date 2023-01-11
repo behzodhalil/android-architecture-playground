97121b4a71b7cbca9a6200442282e77b22a19f8d
Behzod Halil <behzoddev@gmail.com>
2023-01-11 17:03:36 +0900

    Add MainAdapter.kt

---
+++ mvi-orbit/src/main/java/uz/behzoddev/mvi_orbit/ui/adapter/MainAdapter.kt
@@ -0,0 +1,53 @@
+package uz.behzoddev.mvi_orbit.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import uz.behzoddev.mvi_orbit.data.remote.dto.User
+import uz.behzoddev.mvi_orbit.databinding.ItemLayoutBinding
+
+class MainAdapter : ListAdapter<User, MainAdapter.DataViewHolder>(diffUtil) {
+
+    inner class DataViewHolder(private val binding: ItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.apply {
+                tvUserEmail.text = user.email
+                tvUserName.text = user.name
+                Glide.with(ivAvatar.context)
+                    .load(user.avatar)
+                    .into(ivAvatar)
+            }
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        val binding = ItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+
+        return DataViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = currentList.size
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
+        holder.bind(currentList[position])
+
+
+}
