d0ea4aea52c78f2cf15b505ee711bae20d65f00b
Behzod Halil <behzoddev@gmail.com>
2023-01-20 11:11:15 +0900

    Add setting and resources

--- viper-initial/src/main/java/uz/behzoddev/viper_initial/MainActivity.kt
+++ viper-initial/src/main/java/uz/behzoddev/viper_initial/MainActivity.kt
@@ -2,10 +2,39 @@ package uz.behzoddev.viper_initial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzoddev.mvi.data.remote.dto.User
+import uz.behzoddev.viper_initial.ui.contract.ViperContract
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), ViperContract.ViperView {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun showLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun publishData(data: List<User>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showMessage(message: String) {
+        TODO("Not yet implemented")
     }
 }
